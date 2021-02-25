d698e28336c10b3e0207781f879feb3fc5742b67
sandervspl <sandervispoel@hotmail.com>
2021-02-25 18:57:42 +0100

    Update drawCoverFitVideo.ts

--- src/services/drawCoverFitVideo.ts
+++ src/services/drawCoverFitVideo.ts
@@ -1,7 +1,7 @@
 export function drawCoverFitVideo(ctx: CanvasRenderingContext2D, video: HTMLVideoElement): void {
-  const scale = Math.min(ctx.canvas.width / video.videoWidth, ctx.canvas.height / video.videoHeight);
   const vidW = video.videoWidth;
   const vidH = video.videoHeight;
+  const scale = Math.min(ctx.canvas.width / vidW, ctx.canvas.height / vidH);
   const top = ctx.canvas.height / 2 - (vidH / 2) * scale;
   const left = ctx.canvas.width / 2 - (vidW / 2) * scale;
 
