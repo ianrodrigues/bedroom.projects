31b39b375e02b3ec50b155dabacdefc32b5a2afb
sandervspl <sandervispoel@hotmail.com>
2021-03-03 18:18:13 +0100

    Fix opacity issues with canvas, make fixed

--- src/components/common/presentation/FullscreenCanvas/styled.ts
+++ src/components/common/presentation/FullscreenCanvas/styled.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 export const Canvas = styled.canvas<CanvasProps>`
+  position: fixed;
   top: 0;
   left: 0;
   opacity: 0;
+  background-color: #000;
   pointer-events: none;
   transition: opacity 400ms;
 
