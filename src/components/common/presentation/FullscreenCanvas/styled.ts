c7fd7ce1f86bcf628fbfcf0667b09e1c15b98cb7
sandervspl <sandervispoel@hotmail.com>
2021-03-04 21:08:53 +0100

    Add scale to canvas visibility transition

--- src/components/common/presentation/FullscreenCanvas/styled.ts
+++ src/components/common/presentation/FullscreenCanvas/styled.ts
@@ -5,12 +5,14 @@ export const Canvas = styled.canvas<CanvasProps>`
   top: 0;
   left: 0;
   opacity: 0;
+  transform: scale(.97);
   background-color: #000;
   pointer-events: none;
-  transition: opacity 400ms;
+  transition: opacity 400ms, transform 400ms;
 
   ${(props) => props.show && `
     opacity: 1;
+    transform: scale(1);
   `}
 `;
 
