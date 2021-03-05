92d0a49a83f854d03b1e55e4e530c2e7a4a2d813
sandervspl <sandervispoel@hotmail.com>
2021-03-05 22:18:36 +0100

    Update description styling

--- src/components/pages/VideoDetail/styled.ts
+++ src/components/pages/VideoDetail/styled.ts
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 
 export const DescriptionContainer = styled.div`
   display: grid;
-  grid-template-columns: 40% 1fr;
+  grid-template-columns: 20% 20%;
   grid-template-rows: 1fr;
-  gap: 0px 50px;
+  gap: 0px 20px;
   grid-template-areas: "description credits";
   padding-top: 100px;
 
@@ -13,5 +13,6 @@ export const DescriptionContainer = styled.div`
     color: #fff;
     font-family: 'Roboto', sans-serif;
     font-size: 18px;
+    white-space: pre-wrap;
   }
 `;
