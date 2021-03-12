41f934726b92845dca94a69dcf8586ddf244c503
sandervspl <sandervispoel@hotmail.com>
2021-03-12 14:25:29 +0100

    Fix name show issues

--- src/components/pages/Home/components/MediaOverlay/index.tsx
+++ src/components/pages/Home/components/MediaOverlay/index.tsx
@@ -1,7 +1,6 @@
-import * as i from 'types';
 import React from 'react';
 import { enableBodyScroll, disableBodyScroll } from 'body-scroll-lock';
-import { Route, Switch } from 'react-router';
+import { Route, Switch, useLocation } from 'react-router';
 
 import useStore from 'state';
 import MediaTitle from 'common/typography/MediaTitle';
@@ -11,6 +10,7 @@ import { Name, MediaOverlayContainer } from './styled';
 
 const MediaOverlay: React.VFC = () => {
   const state = useStore();
+  const location = useLocation();
   let text: string | undefined = undefined;
 
   if (state.isMenuOpen.L) {
@@ -33,13 +33,19 @@ const MediaOverlay: React.VFC = () => {
     };
   }, []);
 
+  React.useEffect(() => {
+    if (location.pathname === '/' && !state.showName) {
+      state.setShowName(true);
+    }
+  }, [location.pathname]);
+
   return (
     <MediaOverlayContainer>
       <Switch>
         <Route
           path="/"
           exact
-          render={(props) => <Name {...props} show={state.showName}>bedroom</Name>}
+          render={() => <Name show={state.showName}>bedroom</Name>}
         />
       </Switch>
       <MediaTitle visible={state.isFullscreen} side={state.isMenuOpen.L ? 'L' : 'R'}>
