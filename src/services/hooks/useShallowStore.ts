36a646f89cab9de18f6364ca96eae8fa4b6a9c62
sandervspl <sandervispoel@hotmail.com>
2021-12-11 13:56:57 +0100

    Remove function callback

--- src/services/hooks/useShallowStore.ts
+++ src/services/hooks/useShallowStore.ts
@@ -9,32 +9,23 @@ export function useShallowStore<
   K extends keyof i.AppState,
   SK extends SliceKeys<K>,
   R extends Data<K, SK>,
->(key: K, sliceKeys: SKP<SK, K, R>): R {
+>(key: K, sliceKeys: SK): R {
   const selector = React.useCallback((state: i.AppState) => {
     const data: Partial<i.AppState[K]> = {};
 
     // Append slice values to data
-    if (Array.isArray(sliceKeys)) {
-      for (const sliceKey of sliceKeys) {
-        data[sliceKey] = state[key][sliceKey];
-      }
-
-      return data as R;
+    for (const sliceKey of sliceKeys) {
+      data[sliceKey] = state[key][sliceKey];
     }
 
-    // Return cb with state slice
-    return sliceKeys(state[key]);
+    return data as R;
   }, []);
 
   return useStore(selector, shallow);
 }
 
-// type StateKeys = keyof i.AppState;
-
 // Get all keys from state slice K
 type SliceKeys<K extends keyof i.AppState> = (keyof i.AppState[K])[];
-
-type SKP<SK, K extends keyof i.AppState, R> = SK | ((state: i.AppState[K]) => R)
 
 // Get key/value pairs for keys SliceKeys SK of slice K
 type Data<K extends keyof i.AppState, SK extends SliceKeys<K>> = {
